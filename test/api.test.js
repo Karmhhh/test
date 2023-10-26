cdb946955e383fbea2fa2308a4d592ff4e3e1df4
Carmela Fiore <carmela.fiore@sipligroup.com>
2023-10-26 11:45:04 +0200

    _

--- test/api.test.js
+++ test/api.test.js
@@ -2,24 +2,24 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
-describe('GET /api/v1', () => {
+describe('GET /api/Auth', () => {
   it('responds with a json message', (done) => {
     request(app)
       .get('/api/v1')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, {
-        message: 'API - 👋🌎🌍🌏'
+        message: 'API '
       }, done);
   });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1/emojis')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
-  });
-});
+// describe('GET /api/v1/emojis', () => {
+//   it('responds with a json message', (done) => {
+//     request(app)
+//       .get('/api/v1/emojis')
+//       .set('Accept', 'application/json')
+//       .expect('Content-Type', /json/)
+//       .expect(200, ['😀', '😳', '🙄'], done);
+//   });
+// });

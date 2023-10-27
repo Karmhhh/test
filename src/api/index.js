b2d5a49cc83b8683d4d49c97d39c696f0be120ef
Carmela Fiore <carmela.fiore@sipligroup.com>
2023-10-27 14:14:06 +0200

    _

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
 // const tok = require('./tok');
+const sleep = async (milliseconds) => {
+  // eslint-disable-next-line no-promise-executor-return
+  await new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   res.json({
     status: '200, ok',
   });
-  setInterval(() => { res.redirect('/Auth/login'); }, 5000);
+  await sleep(5000);
+  res.redirect('/Auth/login');
 });
 
 // router.use('/Auth', tok);

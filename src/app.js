931651e4c15bdf5e14a36b00e3eab75ca8ecd941
Carmela Fiore <carmela.fiore@sipligroup.com>
2023-10-27 15:42:34 +0200

    _

--- src/app.js
+++ src/app.js
@@ -29,8 +29,7 @@ app.get('/login', (req, res) => {
 
 app.post('/auth', (req, res) => {
   // console.log('Got body:', req.body);
-  res.sendStatus(200);
-  res.json(req.body);
+  res.status(200).json({ status: 'ok' });
 });
 
 // app.use('/auth', api);

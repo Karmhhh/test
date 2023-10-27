882d4e555ac3d54209fbf2483ca77653c31bb9f8
Carmela Fiore <carmela.fiore@sipligroup.com>
2023-10-27 14:05:37 +0200

    _

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    status: '200, ok',
-  });
+  // res.json({
+  //   status: '200, ok',
+  // });
   res.redirect('/Auth/login');
 });
 
